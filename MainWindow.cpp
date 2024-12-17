953eb0450e72f6bb030cf006636ddc3d187c8525
Andrei Fursik <andrey745fa@gmail.com>
2024-12-17 16:41:58 +0300

    Feat[add]: add elements to play with
    - Add a lot of elements to test their signals and slots
    - Test animation in frame element

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,9 +1,41 @@
 #include "MainWindow.h"
+#include <QPropertyAnimation>
+#include <QGraphicsOpacityEffect>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    ui.comboBox->addItem("item1", 1);
+    ui.comboBox->addItem("item2", 2);
+    ui.comboBox->addItem("item3", 3);
+    ui.comboBox->addItem("item4", 4);
+    ui.comboBox->addItem("item5", 5);
+
+    QDoubleValidator* validator = new QDoubleValidator(-500.0, 500.0, 4, this); 
+    ui.lineEdit->setValidator(validator);
+
+    connect(ui.lineEdit, &QLineEdit::textEdited, this, &MainWindow::onTextChanged);
+    connect(ui.dial, &QDial::sliderMoved, this, &MainWindow::onSliderMoved);
+
+    ui.spinBox->setMinimum(-100);
+    ui.spinBox->setMaximum(100);
+    ui.spinBox->setSingleStep(1);
+
+    ui.frame->setStyleSheet("background-color: black;");
+
+
+    QGraphicsColorizeEffect* colorEffect = new QGraphicsColorizeEffect(ui.frame);
+    ui.frame->setGraphicsEffect(colorEffect);
+
+    QPropertyAnimation* animationColor = new QPropertyAnimation(colorEffect, "color");
+    animationColor->setDuration(1000);
+    animationColor->setStartValue(QColor(255, 255, 255)); 
+    animationColor->setEndValue(QColor(255, 0, 0));
+    animationColor->start(QAbstractAnimation::DeleteWhenStopped); // јвтоматическое удаление после завершени€
+
+    connect(ui.horizontalSlider, &QSlider::valueChanged, this, &MainWindow::onHorizontalSliderChanged);
 }
 
 MainWindow::~MainWindow()
--- MainWindow.h
+++ MainWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <qmessagebox.h>
 #include "ui_MainWindow.h"
 
 class MainWindow : public QMainWindow
@@ -10,6 +11,74 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void onCheckBoxChanged(int state)
+    {
+        QMessageBox::information(NULL
+            , "Checkbox"
+            , QString("Checkbox changed!\nIt's state: %1").arg(state == 2 ? "Checked" : "Unchecked"));
+    }
+
+    void onRadioButtonChanged(bool state)
+    {
+        QMessageBox::information(NULL
+            , "Radio Button"
+            , QString("Radio button changed!\nIt's state: %1").arg(state));
+    }
+
+    void onCommandLinkButtonClicked()
+    {
+        QMessageBox::information(NULL
+            , "Command Link Button"
+            , QString("Command Link Button pressed!"));
+    }
+
+    void onTextChanged(const QString& text)
+    {
+        int pos = 0;
+        QString tempText = text;
+        QValidator::State state = ui.lineEdit->validator()->validate(tempText, pos);
+
+        switch (state) 
+        {
+            case QValidator::Intermediate:
+                ui.lineEdit->setStyleSheet("color: blue;");
+                break;
+            case QValidator::Invalid:
+                ui.lineEdit->setStyleSheet("color: red;");
+                break;
+            default:
+                ui.lineEdit->setStyleSheet("color: black;");
+                break;
+        }
+    }
+
+    void onSpinValueChanged(int number)
+    {
+        if (number <= ui.spinBox->minimum() / 2)
+        {
+            ui.spinBox->setStyleSheet("color: red;");
+        }
+        else if (number > ui.spinBox->minimum() / 2 && number <= 0)
+        {
+            ui.spinBox->setStyleSheet("color: blue;");
+        }
+        else
+        {
+            ui.spinBox->setStyleSheet("color: green;");
+        }
+    }
+
+    void onSliderMoved(int num)
+    {
+        ui.lcdNumber->display(num);
+    }
+
+    void onHorizontalSliderChanged(int num)
+    {
+        ui.progressBar->setValue(num);
+    }
 
 private:
     Ui::MainWindowClass ui;
